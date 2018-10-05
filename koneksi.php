ddf9ce59c4b137b6abdafb1076a505aa00813b19
achileusc <43493889+achileusc@users.noreply.github.com>
2018-10-05 10:06:32 +0700

    Update koneksi.php
    Achileus Valentino Carada
    6701170079
    D3MI4104

--- koneksi.php
+++ koneksi.php
@@ -1,3 +1,8 @@
 <?php
+$localhost = "localhost";
+$username = "root";
+$password = "";
+$db = "mahasiswa";
 
+$conn = mysqli_connect($host, $username, $password, $db) or die ("Koneksi Gagal...");
 ?>
