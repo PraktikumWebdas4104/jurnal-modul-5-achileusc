95cfa2fdc3a54e59c0b284e8dfc3697b6ee85a11
achileusc <43493889+achileusc@users.noreply.github.com>
2018-10-05 10:12:20 +0700

    Update komentar.php
    Achileus Valentino Carada
    6701170079
    D3MI4104

--- komentar.php
+++ komentar.php
@@ -1,3 +1,26 @@
+<form method="post">
+	<tr>
+		<td>Komentar </td>
+		<td>:</td>
+		<td><input type="text" name="komen"></td>
+	</tr>
+	<tr>
+		<td><input type="submit" name="kirim"></td>
+	</tr>
+</form>
+
 <?php
+
+if (isset($_POST['kirim'])) {
+	$komen = $_POST['komen'];
+	if (str_word_count($komen)>=5) {
+		echo $komen;
+		# code...
+	}elseif (str_word_count($komen)<5) {
+		echo "Salah";
+		# code...
+	}
+	# code...
+}
 
 ?>
