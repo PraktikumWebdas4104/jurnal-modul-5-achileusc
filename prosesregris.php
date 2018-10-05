ab30d711278bc5c3e06b058c56d4fe88e7ab4de9
achileusc <43493889+achileusc@users.noreply.github.com>
2018-10-05 10:05:47 +0700

    Update prosesregris.php
    Achileus Valentino Carada
    6701170079
    D3MI4104

--- prosesregris.php
+++ prosesregris.php
@@ -1,4 +1,47 @@
 <?php
 
+$localhost = "localhost";
+$username = "root";
+$password = "";
+$db = "mahasiswa";
+
+$conn = mysqli_connect($host, $username, $password, $db) or die ("Koneksi Gagal...");
+
+if (isset($_POST['kirim'])) {
+	$nim = $_POST['nim'];
+	$nama = $_POST['nama'];
+	$email = $_POST['email'];
+	$cekemail = "@gmail.com";
+	$cariemail = strpos($email, $cekemail);
+
+
+	$nim = $_POST['nim'];
+	$nama = $_POST['nama'];
+	$email = $_POST['email'];
+	$cekemail = "@gmail.com";
+	$cariemail = strpos($email, $cekemail);
+
+	if (strlen($nim)==10){
+		if (strlen($nama)<25) {
+			if (strpos($email, "@gmail.com")) {
+				$sql = $conn->query("INSERT INTO `data_mhs`(`nim`,`nama`,`email`)VALUES('$nim','$nama','$email')");
+				echo "Registrasi Berhasil...";
+				# code...
+			}else{
+				echo "Gagal... email tidak sesuai harus '@gmail.com'";
+			}
+			
+			# code...
+		}else{
+			echo "Gagal... Nama tidak sesuai";
+			
+		
+		# code...
+		}
+	}else{
+		echo "Gagal... NIM tidak sesuai";
+	}
+	# code...
+}
 
 ?>
