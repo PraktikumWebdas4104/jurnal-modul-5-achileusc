d76cf51c240954683eff7c7e7e11691643d3aebf
achileusc <43493889+achileusc@users.noreply.github.com>
2018-10-05 09:58:11 +0700

    Update regristrasi.php
    Achileus Valentino Carada
    6701170079
    D3MI4104

--- regristrasi.php
+++ regristrasi.php
@@ -5,7 +5,28 @@
 </head>
 <body>
 
-
+<form method="POST">
+	<table>
+		<tr>
+			<td>NIM </td>
+			<td>:</td>
+			<td><input type="text" name="nim" length="10"></td>
+		</tr>
+		<tr>
+			<td>Nama </td>
+			<td>:</td>
+			<td><input type="text" name="nama" length="25"></td>
+		</tr>
+		<tr>
+			<td>Email </td>
+			<td>:</td>
+			<td><input type="email" name="email"></td>
+		</tr>
+		<tr>
+			<td><input type="submit" name="kirim" value="Kirim"></td>
+		</tr>
+	</table>
+</form>
 
 </body>
 </html>
